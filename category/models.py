85efb31241ebba1eafc7143998de9cf8d6c1af81
Ei Ei Soe lae Wai <050312julia@gmail.com>
2025-01-04 19:25:53 +0800

    store and cart functionality

--- category/models.py
+++ category/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique = True)
-    slug = models.SlugField(max_length=100,unique = True)
+    slug = models.SlugField(max_length=100,unique = True) #URL of Category
     description = models.CharField(max_length=200, blank =True)
     cat_image = models.ImageField(upload_to='photos/categories',blank=True)
 
